01aebde2a1918d08738592d9fbc09ab062ed4cbb
Sam Steingold <sds@gnu.org>
2017-11-24 11:48:10 -0500

    improve code readability according to Jorgen

--- elpy.el
+++ elpy.el
@@ -1593,7 +1593,8 @@ with a prefix argument)."
                              " --ignore="
                            " --exclude=")
                          (mapconcat #'identity
-                                    (elpy-project-ignored-directories) ","))
+                                    (elpy-project-ignored-directories)
+                                    ","))
                       "")))
     (compilation-start (concat python-check-command
                                " "
